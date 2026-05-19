9303ac76358da8ef55b55561cda426f9b7d937c2
bida25-478 <bida25-478@thuto.bac.ac.bw>
2026-05-19 08:33:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,13 +10,3 @@ window.onscroll = () => {
     menu.classList.remove('fa-times')
     navbar.classList.remove('active')
 }
-
-function loader(){
-    document.querySelector('.loader-container').classList.add('.fade-out');
-}
-
-function fadeOut(){
-    setInterval(loader, 3000);
-}
-
-window.onload=fadeOut();
